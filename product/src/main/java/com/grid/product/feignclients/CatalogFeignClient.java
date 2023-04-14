e3c237854bda0f10597b273928b2303d2fbc2201
LuisCastilloGD <lcastillo@griddynamics.com>
2023-04-14 17:17:16 -0600

    add fallback methods

--- product/src/main/java/com/grid/product/feignclients/CatalogFeignClient.java
+++ product/src/main/java/com/grid/product/feignclients/CatalogFeignClient.java
@@ -1,14 +1,16 @@
 package com.grid.product.feignclients;
 
 import com.grid.product.config.CustomFeignConfiguration;
+import com.grid.product.exceptions.ProductNotAvailableException;
 import com.grid.product.models.Article;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "catalog-application", configuration = CustomFeignConfiguration.class)
+@FeignClient(name = "catalog-application", configuration = CustomFeignConfiguration.class, fallback = CatalogFeignClient.Fallback.class)
 public interface CatalogFeignClient {
 
     @GetMapping("/catalog/articles/id/{uniqueId}")
@@ -17,4 +19,24 @@ public interface CatalogFeignClient {
     @GetMapping("/catalog/articles/sku/{sku}")
     List<Article> getProductBySku(@PathVariable("sku") String sku);
 
+    @Component
+    class Fallback implements CatalogFeignClient {
+
+        @Override
+        public Article getProductByUniqueId(final String uniqueId) {
+            return Article.builder()
+                    .uniqId("null")
+                    .sku("null")
+                          .build();
+        }
+
+        @Override
+        public List<Article> getProductBySku(final String sku) {
+            return List.of(Article.builder()
+                                   .uniqId("null")
+                                   .sku("null").build());
+        }
+    }
+
 }
+
--- product/src/main/java/com/grid/product/feignclients/InventoryFeignClient.java
+++ product/src/main/java/com/grid/product/feignclients/InventoryFeignClient.java
@@ -1,16 +1,32 @@
 package com.grid.product.feignclients;
 
 import com.grid.product.config.CustomFeignConfiguration;
+import com.grid.product.models.Article;
 import com.grid.product.models.Item;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "inventory-application", configuration = CustomFeignConfiguration.class)
+import java.util.List;
+
+@FeignClient(name = "inventory-application", configuration = CustomFeignConfiguration.class ,fallback = InventoryFeignClient.Fallback.class)
 public interface InventoryFeignClient {
 
     @GetMapping("/inventory/items/{uniqueId}")
     Item getItemByUniqueId(@PathVariable("uniqueId") String uniqueId);
 
+    @Component
+    class Fallback implements InventoryFeignClient {
+        @Override
+        public Item getItemByUniqueId(final String uniqueId) {
+            return Item.builder()
+                    .uniqId("null")
+                    .availability(1L)
+                       .build();
+        }
+    }
 
 }
